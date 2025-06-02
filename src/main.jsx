c1a1fd931283b58c18c2493eb8903c3566635476
EDiaz210 <elkin.diaz@epn.edu.ec>
2025-06-02 12:26:12 -0500

    Segundo commit

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,7 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  //<StrictMode>
+  <StrictMode>
     <App />
-  //</StrictMode>, //recomendable usar en produccion no lo olvides
-  
+  </StrictMode>
 )
